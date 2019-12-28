7a4165d015e92a24570320a2b675f3f71ec80b1a
naaolshupare17 <32284725+naaolshupare17@users.noreply.github.com>
2019-12-27 21:04:42 -0800

    Add files via upload

---
+++ multiarray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+  int arr[2][2][2] = { {1,2}, {3,4}, {5,6}, {7,8}};
+
+  int a = 0, b = 0;
+
+  string loc(int a, b) {
+
+    printf(" %d, %d\n ", a, b);
+
+  }
+
+  
